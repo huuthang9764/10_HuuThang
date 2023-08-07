a7cedeac24bcede2daa445580ce51b52adfc75f5
Nguyễn Hữu Thắng <76506824+huuthang9764@users.noreply.github.com>
2023-08-07 21:19:29 +0700

    0708 - INSERT INTO

--- Database/Extra_Assignment.sql
+++ Database/Extra_Assignment.sql
@@ -29,7 +29,19 @@ VALUES ('01','Nguyen Huu Thang',DATE'2008-11-11','male','9','13','43','38','noco
 ('07','Pham Vu Hoa',DATE'2022-10-05','male','5','18','30','38','nocomment','37'),
 ('08','Huynh Phat Huy',DATE'2022-10-06','unknown','10','6','20','38','nocomment','38'),
 ('09','Duong Ngoc Nguyen',DATE'2022-10-07','unknown','2','6','36','38','nocomment','39'),
-('10','Nguyen Anh Minh',DATE'2022-10-08','male','7','6','39','38','nocomment','40');
+('10','Nguyen Anh Minh',DATE'2022-10-08','male','7','6','39','38','nocomment','40'),
+
+-- ('John Doe', '1995-03-10', 'male', 15, 18, 40, 'Class A', 'Good performance', 'john.doe@example.com'),
+-- ('Jane Smith', '1998-09-25', 'female', 17, 16, 48, 'Class B', 'Exceeds expectations', 'jane.smith@example.com'),
+-- ('Michael Johnson', '1997-07-03', 'male', 14, 19, 42, 'Class A', 'Solid effort', 'michael.johnson@example.com'),
+-- ('Emily Brown', '1996-11-12', 'female', 16, 17, 46, 'Class C', 'Improvement needed', 'emily.brown@example.com'),
+-- ('David Lee', '1999-02-18', 'male', 18, 15, 45, 'Class B', 'Impressive skills', 'david.lee@example.com'),
+-- ('Sophia Williams', '2000-04-30', 'female', 15, 18, 50, 'Class A', 'Great potential', 'sophia.williams@example.com'),
+-- ('Jacob Martinez', '1997-08-05', 'male', 12, 20, 38, 'Class C', 'Needs extra attention', 'jacob.martinez@example.com'),
+-- ('Olivia Johnson', '1998-01-22', 'female', 19, 14, 49, 'Class B', 'Outstanding performer', 'olivia.johnson@example.com'),
+-- ('William Davis', '1996-12-08', 'male', 13, 16, 41, 'Class A', 'Showing improvement', 'william.davis@example.com'),
+-- ('Emma Smith', '2001-06-17', 'female', 15, 17, 44, 'Class C', 'Working hard', 'emma.smith@example.com');
+
 
 -- Exercise 2 : Data Types
 DROP TABLE IF EXISTS `Datatype1`;
@@ -49,8 +61,28 @@ CREATE TABLE `Datatype2` (
     Gender INT CHECK (Gender IN (0 , 1)),
     IsDeletedFlag INT CHECK (IsDeletedFlag IN (0 , 1))
 );
-20:52:05	INSERT INTO Trainee(Full_Name,Birth_Date,Gender,ET_IQ,ET_Gmath,ET_English,Training_Class,Evaluation_Notes,VTI_Account) VALUES ('Nguyễn Hữu Thắng',DATE'2008-11-11','male','9','13','43','38','nocomment','10' ) ,  ('Vũ Minh Hưng',DATE'2022-09-30','male','10','6','35','38', 'nocomment','11'), ('Nguyễn Thị Thu Hà',DATE'2022-10-01', 'female','8','9','10','38','nocomment','12'), ('Nguyễn Văn Thưởng',DATE'2022-10-02', 'male', '12','20','31','38','nocomment','13'), ('Vi Xuân Dũng',DATE'2022-10-03','male','11','6','13','38','nocomment','14'), ('Nguyễn Thị Khả Nhi',DATE'2022-10-04', 'female','10','14','44','38','nocomment','15'), ('Phạm Vũ Hòa',DATE'2022-10-05','male','5','18','30','38','nocomment','16'), ('Thái Tín Khang',DATE'2022-10-06','male','10','6','20','38','nocomment','17'), ('Huỳnh Nhật Khang',DATE'2022-10-07','male','2','6','36','38','nocomment','18'), ('Nguyễn Anh Minh',DATE'2022-10-08','male','7','6','39','38','nocomment','19')	Error Code: 1062. Duplicate entry '10' for key 'VTI_Account'	0.000 sec
-20:56:56	INSERT INTO `Trainee`(Full_Name,Birth_Date,Gender,ET_IQ,ET_Gmath,ET_English,Training_Class,Evaluation_Notes,VTI_Account) VALUES ('Nguyễn Hữu Thắng',DATE'2008-11-11','male','9','13','43','38','nocomment','21' ) ,  ('Vũ Minh Hưng',DATE'2022-09-30','male','10','6','35','38', 'nocomment','22'), ('Nguyễn Thị Thu Hà',DATE'2022-10-01', 'female','8','9','10','38','nocomment','23'), ('Nguyễn Văn Thưởng',DATE'2022-10-02', 'male', '12','20','31','38','nocomment','24'), ('Vi Xuân Dũng',DATE'2022-10-03','male','11','6','13','38','nocomment','25'), ('Nguyễn Thị Khả Nhi',DATE'2022-10-04', 'female','10','14','44','38','nocomment','26'), ('Phạm Vũ Hòa',DATE'2022-10-05','male','5','18','30','38','nocomment','27'), ('Thái Tín Khang',DATE'2022-10-06','male','10','6','20','38','nocomment','28'), ('Huỳnh Nhật Khang',DATE'2022-10-07','male','2','6','36','38','nocomment','29'), ('Nguyễn Anh Minh',DATE'2022-10-08','male','7','6','39','38','nocomment','30')	Error Code: 1062. Duplicate entry '21' for key 'VTI_Account'	0.000 sec
 
-Error Code: 1062. Duplicate entry '21' for key 'VTI_Account'	0.000 sec
+
+
+DROP DATABASE IF EXISTS `Fresher_management`;
+CREATE DATABASE  `Fresher_management`;
+USE `Fresher_management`;
+
+DROP TABLE IF EXISTS `Trainee`;
+CREATE TABLE `Trainee` (
+    TraineeID INT AUTO_INCREMENT PRIMARY KEY,
+    Full_Name NVARCHAR(255) NOT NULL, -- NVARCHAR thay vi VARCHAR
+    Birth_Date DATE NOT NULL,
+    Gender ENUM('male', 'female', 'unknown') NOT NULL,
+    ET_IQ TINYINT CHECK (ET_IQ BETWEEN 0 AND 20), -- TINYINT thay vi INT
+    ET_Gmath TINYINT CHECK (ET_Gmath BETWEEN 0 AND 20), -- TINYINT thay vi INT
+    ET_English TINYINT CHECK (ET_English BETWEEN 0 AND 50), -- TINYINT thay vi INT
+    Training_Class VARCHAR(50),
+    Evaluation_Notes TEXT
+);
+
+ALTER TABLE `Trainee` 
+ADD COLUMN VTI_Account VARCHAR(100) NOT NULL UNIQUE;  
+
+
 
